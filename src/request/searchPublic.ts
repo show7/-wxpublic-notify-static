ba796cc9915736c4251467815dbd28c695c2472a
alexyu <alexyu@iquanwai.com>
2019-04-05 07:05:18 +0800

    dev

--- src/request/searchPublic.ts
+++ src/request/searchPublic.ts
@@ -3,6 +3,6 @@ import Vue from 'vue'
 
 const validateStatus = (status:number)=>status >= 200 && status < 300 || status == 700
 export default{
-  searchPublic: (text: any) => Vue.axios.get('/api/webwechat/search', {params: {weChatName: text,validateStatus}}),
+  searchPublic: (text: any) => Vue.axios.get('/api/webwechat/search', {params: {weChatName: text},validateStatus}),
   recommandPublic: (params:object) => Vue.axios.post('/api/webwechat/recommend', params)
 }
