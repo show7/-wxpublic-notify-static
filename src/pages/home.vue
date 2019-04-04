77be10a621d2bfb3da41d6e9ac3eaf22af486d69
alexyu <alexyu@iquanwai.com>
2019-04-05 02:44:39 +0800

    dev

---
+++ src/pages/home.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  .home-component-wrap
+    Search(@input = "searchChange")
+    transition(name="fade")
+      router-view
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import Search from '@/components/search/Search.vue'
+import Popup from '@/components/popup/Popup.vue'
+
+import { State, Action } from 'vuex-class'
+import { constants } from 'fs'
+
+@Component({
+  name: 'Home',
+  components: {
+    Search,
+    Popup
+  }
+})
+export default class Home extends Vue {
+  @Action changeInputText: (val: string) => void
+  // @Action getHome: (params: any) => void
+  // Alert(params: number) {
+  //   alert(params == 1)
+  // }
+  searchChange(val: any) {
+    console.log(this.$route.name)
+    if (val !== '') this.changeInputText(val)
+    if (val === '' && this.$route.name !== 'Home') {
+      this.$router.replace({ path: '/' })
+    } else if (this.$route.name !== 'searchResult') {
+      this.$router.replace({ path: '/searchResult' })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import '../style/common.less';
+
+.home-component-wrap {
+}
+</style>
