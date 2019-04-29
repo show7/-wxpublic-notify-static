599c3d4ee79b8253478f8b5dc8fbc6670e5612d2
alexyu <alexyu@iquanwai.com>
2019-04-29 22:29:02 +0800

    dev

--- src/pages/classifyList/ClassifyList.vue
+++ src/pages/classifyList/ClassifyList.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   ScrollView
-    .topic(v-if="isKOL") 下面是{{kolname}}订阅的公众号，小新已经为您关注啦！ 每天为您推送当日的公众号更新，优化您的阅读体验～
+    .topic2(class="topic" v-if="isKOL") 下面是{{kolname}}订阅的公众号，小新已经为您关注啦！ 每天为您推送当日的公众号更新，优化您的阅读体验～
     .topic(v-else) 感谢关注，小新为您推荐～
     div(v-if="isKOL")
       .contentFlex
@@ -15,17 +15,18 @@
               div {{item.isSubscribe ? '已订阅' : '订阅'}}
     div(v-else)
       .kindContent
-        .kindsList(v-for='(res,i) in allList' :key='index')
+        .kindsList(v-for='(res,i) in allList' :key='i')
           .kindsTit {{res.categoryName}}
           .contentFlex
             .public-address-component-list(v-for='(item,index) in res.weChatPublicVOs' :key='index')
-              img(class="public-address-head-img" :src="item.avatar")
-              div(class="public-address-info")
-                div(class="public-address-title" v-html='item.weChatName')
-                div(class="public-address-introduction") {{item.description}}
-              div(v-if='item.isSubscribe !== undefined' :class="['public-address-subscribe',subscribe(item.isSubscribe)]" @click='setnormalSubscribeStatus(item,index)') 
-                i(v-if='item.isSubscribe' class="iconfont iconduihao")
-                div {{item.isSubscribe ? '已订阅' : '订阅'}}
+              div
+                img(class="public-address-head-img" :src="item.avatar")
+                div(class="public-address-info")
+                  div(class="public-address-title" v-html='item.weChatName')
+                  div(class="public-address-introduction") {{item.description}}
+                div(v-if='item.isSubscribe !== undefined' :class="['public-address-subscribe',subscribe(item.isSubscribe)]" @click='setnormalSubscribeStatus(item, index, i)') 
+                  i(v-if='item.isSubscribe' class="iconfont iconduihao")
+                  div {{item.isSubscribe ? '已订阅' : '订阅'}}
         //- .public-address-component(v-for='(item,i) in allList' :key='"index"+i' v-show='!showMore' @click="recommend ? confirm((item.weChatName).replace('<strong>','').replace('</strong>',''),item.searchId) :''")
         //-   div
         //-     img(class="public-address-head-img" :src="item.avatar")
@@ -70,6 +71,7 @@ export default class ClassifyList extends Vue {
   isEnd = false
   loading = false
   finished = false
+  private i: number = 0
   private kolname: string = ''
   private isKOL: boolean = false
   private cancelSubPopup: boolean = false
@@ -116,7 +118,7 @@ export default class ClassifyList extends Vue {
     }
   ]
   pathToGuide() {
-    this.$router.push('/guide')
+    this.$router.replace('/guide')
   }
   ok() {
     this.subscribePopup = false
@@ -127,7 +129,7 @@ export default class ClassifyList extends Vue {
       function: '确认取消订阅',
       action: '点击确认取消'
     })
-    this.unsubscribeFnc(this.weChatPublicId, this.index)
+    this.unsubscribeFnc(this.weChatPublicId, this.index, this.i)
   }
   cancel() {
     this.cancelSubPopup = false
@@ -156,7 +158,7 @@ export default class ClassifyList extends Vue {
     // this.subscribePopup = true
     this.allList[index].isSubscribe = true
   } //取消订阅
-  async unsubscribeFnc(weChatPublicId: number, index: number) {
+  async unsubscribeFnc(weChatPublicId: number, index: number, i: number) {
     this.cancelSubPopup = true
     this.CanClick('')
     const res: Ajax.AxiosResponse | any = await unsubscribe.unsubscribe({
@@ -164,9 +166,11 @@ export default class ClassifyList extends Vue {
       weChatPublicId: weChatPublicId
     })
     this.cancelSubPopup = false
-    this.allList[index].isSubscribe = false
+    if (i >= 0) {
+      this.allList[i].weChatPublicVOs[index].isSubscribe = false
+    }
   }
-  setnormalSubscribeStatus(item: any, index: number) {
+  setnormalSubscribeStatus(item: any, index: number, i: number) {
     const { isSubscribe, weChatPublicId } = item
     if (isSubscribe) {
       mark({
@@ -177,16 +181,17 @@ export default class ClassifyList extends Vue {
       this.cancelSubPopup = true
       this.weChatPublicId = weChatPublicId
       this.index = index
+      this.i = i
     } else {
       mark({
         module: '打点',
         function: '订阅按钮',
         action: '订阅按钮点击'
       })
-      this.subscribenormalFnc(weChatPublicId, index)
+      this.subscribenormalFnc(weChatPublicId, index, i)
     }
   }
-  async subscribenormalFnc(weChatPublicId: number, index: number) {
+  async subscribenormalFnc(weChatPublicId: number, index: number, i: number) {
     this.CanClick('')
     const res: Ajax.AxiosResponse | any = await subscribe.subscribe({
       isSearchResult: false,
@@ -194,7 +199,7 @@ export default class ClassifyList extends Vue {
     })
     this.subscribeNum = res.msg
     // this.subscribePopup = true
-    this.allList[index].isSubscribe = true
+    this.allList[i].weChatPublicVOs[index].isSubscribe = true
   }
   setSubscribeStatus(item: any, index: number) {
     const { isSubscribe, weChatPublicId } = item
