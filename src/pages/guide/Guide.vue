39d3b6d7e6de83d7824a2ce0db86442f7422e5dc
alexyu <alexyu@iquanwai.com>
2019-04-30 22:01:34 +0800

    dev

--- src/pages/guide/Guide.vue
+++ src/pages/guide/Guide.vue
@@ -8,8 +8,8 @@
         img(class="bgImg2" src="https://static.iqycamp.com/guideImg-1ej3lf7f.png")
         img(class="stepImg2" src="https://static.iqycamp.com/steptwo-bg31wwdf.png")
         div
-          div(class="boot-step-btn boot-step-2" style="left:20px;right:unset" @click="pathTo('/', '开始订阅！')") 开始订阅！
-          div(class="boot-step-btn boot-step-2" style="right:20px;left:unset" @click="pathTo('/myorder', '阅读文章！')") 阅读文章！
+          div(class="boot-step-btn boot-step-2" style="left:20px;right:unset" @click="pathTo('/', '开始订阅！', 'true')") 开始订阅！
+          div(class="boot-step-btn boot-step-2" style="right:20px;left:unset" @click="pathTo('/myorder', '阅读文章！', 'false')") 阅读文章！
 </template>
 <script lang="ts">
 import Vue from 'vue'
@@ -17,6 +17,7 @@ import { State, Action } from 'vuex-class'
 import Component from 'vue-class-component'
 import Popup from '@/components/popup/Popup.vue'
 import mark from '../../utils/mark'
+import { subscribe } from '../../request'
 @Component({
   name: 'Guide',
   components: {
@@ -26,13 +27,19 @@ import mark from '../../utils/mark'
 export default class Guide extends Vue {
   @Action setNoviceGuideState: (params: any) => void
   @State noviceGuideState: number
-  pathTo(url: string, notice: string) {
+  async pathTo(url: string, notice: string, subscribeIt: string) {
     mark({
       module: '打点',
       function: '引导页',
       action: '着陆引导页',
       memo: notice
     })
+    if (subscribeIt === 'true') {
+      const res: Ajax.AxiosResponse | any = await subscribe.subscribe({
+        isSearchResult: false,
+        weChatPublicId: 17
+      })
+    }
     this.$router.replace(url)
   }
   mounted() {
